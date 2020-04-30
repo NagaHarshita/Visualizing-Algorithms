53fcd77459b3000ba7ef48cc05073be737b2a2d9
nagaharshita <harshitamarupaka@gmail.com>
2020-04-30 12:02:37 +0530

    Added DFS

--- Visualize_Algorithms/Visualising_algorithms/src/app/App.java
+++ Visualize_Algorithms/Visualising_algorithms/src/app/App.java
@@ -5,7 +5,8 @@ import algorithms.*;
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Java");
-        // new Dijkstra();
+        new Dijkstra();
         new BFS();
+        new DFS();
     }
 }
